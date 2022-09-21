f0567f49e3eab0a27849f9248af88ed2d2a3f992
fcms14 <101072783+fcms14@users.noreply.github.com>
2022-09-20 22:06:45 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,76 +1,44 @@
 import { LocalStorageFunctions } from "./storageFunctions/localStorageFunctions.js";
 
 const userContractLocal = new LocalStorageFunctions("@imob");
-const userSheetLocal = new LocalStorageFunctions("@sheet");
-const userInput    = document.querySelectorAll('input');
-let userContract   = loadUserData();
-let bills = userContract ? calcBills(userSheetLocal.load() ?? []) : [];
+const userSheetLocal    = new LocalStorageFunctions("@sheet");
 
-for (const item of userInput) {
-    item.addEventListener('change', validateUserInputs)
-}
-
-function calculateMonthlyRates() {
-    nominalMonthly.innerHTML  = (nominalYearly.value / 12).toFixed(4);
-    cetMonthly.innerHTML      = (cetYearly.value / 12).toFixed(4);
-    efectiveMonthly.innerHTML = (efectiveYearly.value / 12).toFixed(4);
-}
+let userContract = loadUserData();
+let bills        = userContract ? calcBills(userSheetLocal.load() ?? []) : [];
 
 function loadUserData() {
     const userContract = userContractLocal.load();
 
     if (userContract) {
-        userInput[0].value  = userContract.propertyValue;
-        userInput[1].value  = userContract.nominalYearly;
-        userInput[2].value  = userContract.avaliation;
-        userInput[3].value  = userContract.financedValue;
-        userInput[4].value  = userContract.cetYearly;
-        userInput[5].value  = userContract.itbi;
-        userInput[6].value  = userContract.term;
-        userInput[7].value  = userContract.efectiveYearly;
-        userInput[8].value  = userContract.initialDate;
-        userInput[9].value  = userContract.registry;
-        userInput[10].value = userContract.mip;
-        userInput[11].value = userContract.dfi;
-        userInput[12].value = userContract.tsa;
-    
-        calculateMonthlyRates();
+        nominalYearly.value  = userContract.nominalYearly;
+        mip.value = userContract.mip;
+        dfi.value = userContract.dfi;
+        tsa.value = userContract.tsa;
+        propertyValue.value  = userContract.propertyValue;
+        avaliation.value  = userContract.avaliation;
+        financedValue.value  = userContract.financedValue;
+        itbi.value  = userContract.itbi;
+        initialDate.value  = userContract.initialDate;
+        term.value  = userContract.term;
+        registry.value  = userContract.registry;
     }
     return userContract;
 }
 
-function getUserInputs(userInput) {
+function getUserInputs() {
     return {
-        propertyValue: userInput[0].value,
-        nominalYearly: userInput[1].value,
-        avaliation: userInput[2].value,
-        financedValue: userInput[3].value,
-        cetYearly: userInput[4].value,
-        itbi: userInput[5].value,
-        term: userInput[6].value,
-        efectiveYearly: userInput[7].value,
-        initialDate: userInput[8].value,
-        registry: userInput[9].value,
-        mip: userInput[10].value,
-        dfi: userInput[11].value,
-        tsa: userInput[12].value
+        nominalYearly: nominalYearly.value,
+        mip: mip.value,
+        dfi: dfi.value,
+        tsa: tsa.value,
+        propertyValue: propertyValue.value,
+        avaliation: avaliation.value,
+        financedValue: financedValue.value,
+        itbi: itbi.value,
+        initialDate: initialDate.value,
+        term: term.value,
+        registry: registry.value,
     }
-}
-
-function validateUserInputs() {
-    let itens = 0;
-
-    for (const item of userInput) {
-        const { value } = item;
-
-        itens = value ? itens + 1 : itens;
-
-        if (userInput.length == itens) {
-            userContractLocal.add(getUserInputs(userInput));
-            userContract = loadUserData();
-        }
-    }
-    calculateMonthlyRates();
 }
 
 function createTableRows(){
@@ -81,7 +49,7 @@ function createTableRows(){
 	for (let i = 0; i < rows; i++) {
 		const row =	table.insertRow(i);
 
-        for (let cols = 0; cols < 19; cols++) {
+        for (let cols = 0; cols < 17; cols++) {
             row.insertCell(cols);
         }
 	}
@@ -115,6 +83,8 @@ function calcBills (bills = []) {
         
         const extraPayments = bills[i] ? Number(bills[i].extraPayments) : 0;
         const method        = bills[i] ? bills[i].method : "";
+        const paymentDate   = bills[i] ? bills[i].paymentDate ? bills[i].paymentDate : "" : "";
+        const payedValue    = bills[i] ? bills[i].payedValue : 0;
 
         balanceDue    = rowData.newBalance + trValue - extraPayments;
 
@@ -145,12 +115,8 @@ function calcBills (bills = []) {
             extraPayments,
             method,
             reduction,
-
-            extraFee1: 0,
-            extraFee2: 0,
-
-            paymentDate: null,
-            payedValue: 0
+            paymentDate,
+            payedValue
         }
 
         if (bills[i]) {
@@ -166,6 +132,7 @@ function calcBills (bills = []) {
 };
 
 function fillData() {
+    let totalDue = 0 , totalQuota = 0, totalReduction = 0, totalRate = 0, totalTrValue = 0, totalMip = 0, totalDfi = 0, totalTsa = 0, totalPayed = 0;
     if (userContract) {    
         let row = 0;
         while (bills[row].balance >= 0) {
@@ -179,8 +146,18 @@ function fillData() {
             expected.rows[row].cells[10].innerHTML = bills[row].mip.toFixed(2);
             expected.rows[row].cells[11].innerHTML = bills[row].dfi.toFixed(2);
             expected.rows[row].cells[12].innerHTML = bills[row].tsa.toFixed(2);
-            expected.rows[row].cells[15].innerHTML = bills[row].balance.toFixed(2);
-            expected.rows[row].cells[16].innerHTML = bills[row].paymentDate ? "Pago" : "Em Aberto"            
+            expected.rows[row].cells[13].innerHTML = bills[row].balance.toFixed(2);
+            expected.rows[row].cells[14].innerHTML = bills[row].paymentDate ? "Pago" : "Em Aberto";
+            
+            totalDue        += Number(bills[row].total);
+            totalQuota      += Number(bills[row].quota);
+            totalReduction  += Number(bills[row].reduction);
+            totalRate       += Number(bills[row].rateValue);
+            totalTrValue    += Number(bills[row].trValue);
+            totalMip        += Number(bills[row].mip);
+            totalDfi        += Number(bills[row].dfi);
+            totalTsa        += Number(bills[row].tsa);
+            totalPayed      += Number(bills[row].payedValue);
             
             if (bills[row].balance == 0) {
                 break;
@@ -188,25 +165,32 @@ function fillData() {
             row++;
         }
     }
+
+    TDtotalDue.innerHTML = totalDue.toFixed(2); 
+    TDtotalQuota.innerHTML = totalQuota.toFixed(2);
+    TDtotalReduction.innerHTML = totalReduction.toFixed(2);
+    TDtotalRate.innerHTML = totalRate.toFixed(2);
+    TDtotalTrValue.innerHTML = totalTrValue.toFixed(2);
+    TDtotalMip.innerHTML = totalMip.toFixed(2);
+    TDtotalDfi.innerHTML = totalDfi.toFixed(2);
+    TDtotalTsa.innerHTML = totalTsa.toFixed(2);
+    TDtotalPayed.innerHTML = totalPayed.toFixed(2);
 }
 
 function setInputs() { 
-    console.log(bills);
     for (let row = 0; row < expected.rows.length; row++) {
         expected.rows[row].cells[8].innerHTML  = `<input type='number' name="tr" min="0" value=${bills[row].tr.toFixed(8)} id="${row}"> </input>`;
         expected.rows[row].cells[5].innerHTML  = `<input  type='number' name="extraPayments" min="0" value=${bills[row].extraPayments.toFixed(2)} id="${row}"> </input>`;
         expected.rows[row].cells[4].innerHTML  = `<select id='method-${row}' name="methods"> <option value='cut' ${bills[row].method == "cut" ? 'selected' : ''}> Redução </option> <option value="term" ${bills[row].method == "term" ? 'selected' : ''}> Prazo </option> </select>`;
-
-        // todo (extra) ->
-        expected.rows[row].cells[13].innerHTML = `<input  name="extraFee1" type='number' min="0" value=${bills[row].extraFee1.toFixed(2)} id="${row}"> </input>`;
-        expected.rows[row].cells[14].innerHTML = `<input  name="extraFee2" type='number' min="0" value=${bills[row].extraFee2.toFixed(2)} id="${row}"> </input>`;
-        expected.rows[row].cells[17].innerHTML = bills[row].paymentDate ? bills[row].paymentDate.toLocaleDateString() : `<input  name="paymentDate" type='date' id="${row}"> </input>`;
-        expected.rows[row].cells[18].innerHTML = `<input  name="payedValue" type='number' min="0" value=${bills[row].payedValue.toFixed(2)} id="${row}"> </input>`;
+        expected.rows[row].cells[15].innerHTML = `<input  name="paymentDate" type='date' id="${row}" value="${bills[row].paymentDate ?? ''}"> </input>`; // bills[row].paymentDate ? bills[row].paymentDate.toLocaleDateString() : 
+        expected.rows[row].cells[16].innerHTML = `<input  name="payedValue" type='number' min="0" value=${Number(bills[row].payedValue).toFixed(2)} id="${row}"> </input>`;
     }
 
     const trs = document.querySelectorAll('[name="tr"]');
     const extraPayments = document.querySelectorAll('[name="extraPayments"]');
-    // const methods       = document.querySelectorAll('[name="methods"]');
+    const paymentDates  = document.querySelectorAll('[name="paymentDate"]');
+    const payedValues   = document.querySelectorAll('[name="payedValue"]');
+    const methods       = document.querySelectorAll('[name="methods"]');
     
     for (const item of trs) {
         item.addEventListener('change', (e) => {
@@ -229,13 +213,49 @@ function setInputs() {
             fillData();
         });
     }
+
+    for (const item of methods) {
+        item.addEventListener('change', (e) => {
+            const method      = e.target.value;
+            const id          = e.target.id.substring(7);
+            bills[id].method  = method;    
+
+            bills = calcBills(bills);
+            fillData();
+        });
+    }
+
+
+    for (const item of paymentDates) {
+        item.addEventListener('change', (e) => {
+            const {id, value} = e.target;
+            bills[id].paymentDate = value;
+    
+            bills = calcBills(bills);
+        });
+    }
+
+    for (const item of payedValues) {
+        item.addEventListener('change', (e) => {
+            const {id, value} = e.target;
+            bills[id].payedValue = value;
+    
+            bills = calcBills(bills);
+            fillData();
+        });
+    }
 }
 
 function updateTable(){
-    createTableRows();
-    bills = calcBills();
-    fillData();
-    setInputs();
+    if (confirm("Você tem certeza que deseja atualizar os dados? \nEsta ação apagará os valores inseridos na tabela abaixo.")) {
+        userContractLocal.add(getUserInputs());
+        userContract = loadUserData();
+        createTableRows();
+        bills = calcBills();
+        fillData();
+        setInputs();
+    }
+
 }
 
 createTableRows();
